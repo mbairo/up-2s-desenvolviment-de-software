df1914351e80d6e7b602129bab09f90005573b06
mBairo <bairo.marcos@gmail.com>
2022-04-06 14:08:32 -0300

    acrescenta loop no menu e melhorias na sintaxe do ex2

--- trabalho01/ex2/Calculadora.java
+++ trabalho01/ex2/Calculadora.java
@@ -17,56 +17,65 @@ class Calculo {
     public static double divisao(int numero1, int numero2) {
         return ((double) numero1 / numero2);
     }
-
 }
 
 public class Calculadora {
-
     public static void main(String[] args) {
-
-        // declara varíaveis
+        
         int opcao;
         int numero1;
         int numero2;
 
-        // mostar opções para o usuário
-        Scanner input = new Scanner(System.in);
+        Scanner scannerOpcao = new Scanner(System.in);
+
+        System.out.println("\n#########################################");
         System.out.println("*Escolha a opção desejada*");
         System.out.println("Digite 1 para soma: ");
         System.out.println("Digite 2 para subração: ");
         System.out.println("Digite 3 para mutiplicação: ");
         System.out.println("Digite 4 para divisão: ");
-        System.out.println("0. Sair");
-        System.out.println("Operação: ");
+        System.out.println("Informe qualquer outro caractere para SAIR. ");
+        System.out.print("\nOperação: ");
 
-        // Lê a opção escolhida
-        opcao = input.nextInt();
+        opcao = scannerOpcao.nextInt();
+
         
         while (opcao != 0 && opcao < 5) {
-            Scanner input1 = new Scanner(System.in);
+            Scanner scannerNumero = new Scanner(System.in);
 
-            System.out.println("Informe o primeiro número: ");
-            numero1 = input1.nextInt();
-            System.out.println("Informe o segundo número: ");
-            numero2 = input1.nextInt();
-
+            System.out.println("\nInforme o primeiro número: ");
+            numero1 = scannerNumero.nextInt();
+            System.out.println("\nInforme o segundo número: ");
+            numero2 = scannerNumero.nextInt();
             
+            System.out.println("\n***************************************");
             if (opcao == 1) {
                 int resultado = Calculo.soma(numero1, numero2);
-                System.out.printf("\nO resultado da soma é: %d\n", resultado);
+                System.out.print("O resultado da soma entre " + numero1 + " e " + numero2 + " é: " + resultado);
             } else if (opcao == 2) {
                 int resultado = Calculo.subtracao(numero1, numero2);
-                System.out.printf("\nO resultado da subtração é: %d\n", resultado);
+                System.out.print("O resultado da subtração entre " + numero1 + " e " + numero2 + " é: " + resultado);
             } else if (opcao == 3) {
                 int resultado = Calculo.multiplicacao(numero1, numero2);
-                System.out.printf("\nO resultado da multiplicação é: %d\n", resultado);
+                System.out.print("O resultado da multiplicação entre " + numero1 + " e " + numero2 + " é: " + resultado);
             } else if (opcao == 4) {
                 double resultado = Calculo.divisao(numero1, numero2);
-                System.out.println(resultado);
-                // System.out.printf("\nO resultado da subtração é: %d\n", resultado);
+                System.out.print("O resultado da divisão entre " + numero1 + " e " + numero2 + " é: " +resultado);
             }
+            
+            System.out.println("\n***************************************");
+            System.out.println("\n");
 
+            System.out.println("\n#########################################");
+            System.out.println("*Escolha a opção desejada*");
+            System.out.println("Digite 1 para soma: ");
+            System.out.println("Digite 2 para subração: ");
+            System.out.println("Digite 3 para mutiplicação: ");
+            System.out.println("Digite 4 para divisão: ");
+            System.out.println("Informe qualquer outro caractere para SAIR. ");
+            System.out.print("\nOperação: ");
+    
+            opcao = scannerOpcao.nextInt();
         }
     }
-
 }
