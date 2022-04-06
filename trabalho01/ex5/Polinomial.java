67fc14449982d436c6671ace71092f40e1a995be
mBairo <bairo.marcos@gmail.com>
2022-04-06 17:33:19 -0300

    cria ex5 - apenas o inicio

---
+++ trabalho01/ex5/Polinomial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Polinomial
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Informe o grau da função: ");
+        int grau = scanner.nextInt();
+        int [] graudaFuncao = new int[grau];
+
+        do {
+          
+            System.out.print("Informe o coeficiente: ");
+            int grau = scanner.nextInt();
+    
+        } while (condition);
+
+
+
+        System.out.print("Informe os coeficiente(s): ");
+        int grau = scanner.nextInt();
+        System.out.print("Informe o grau da função: ");
+        int grau = scanner.nextInt();
+        System.out.print("Informe o grau da função: ");
+        int grau = scanner.nextInt();
+
+    }
