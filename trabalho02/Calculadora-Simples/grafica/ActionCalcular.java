f7b4989179dc2c2ada6854f5b7ee5d7b44b4fc41
mbairo <bairo.marcos@gmail.com>
2022-06-03 20:03:46 -0300

    add trabalho2 - ex1

---
+++ trabalho02/Calculadora-Simples/grafica/ActionCalcular.java
@@ -0,0 +1,44 @@
+package grafica;
+ 
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import logica.Calculadora;
+
+public class ActionCalcular extends AbstractAction {
+    private JTextField txtx, txty;
+    private JComboBox<String> cbxOperacao;
+
+    public ActionCalcular(JTextField txtx, JTextField txty, JComboBox<String>cbxOperacao){
+        this.txtx = txtx;
+        this.txty = txty;
+        this.cbxOperacao = cbxOperacao;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+        double x = Double.parseDouble(txtx.getText()); // parseDOUBLE de String para double
+        double y = Double.parseDouble(txty.getText());
+        Calculadora calc = new Calculadora(x, y);
+
+        if (cbxOperacao.getSelectedItem().equals("Soma")) { 
+            JOptionPane.showMessageDialog(null, String.format("O resultado de %.2f + %.2f é %.2f", x, y, calc.somar()), "Calculadora simples", JOptionPane.INFORMATION_MESSAGE); // INFORMATION é um icone no caso "i"
+        } 
+        if (cbxOperacao.getSelectedItem().equals("Subtração")) { 
+            JOptionPane.showMessageDialog(null, String.format("O resultado de %.2f - %.2f é %.2f", x, y, calc.subtrair()), "Calculadora simples", JOptionPane.INFORMATION_MESSAGE); // INFORMATION é um icone no caso "i"
+        } 
+        if (cbxOperacao.getSelectedItem().equals("Multiplicação")) { 
+            JOptionPane.showMessageDialog(null, String.format("O resultado de %.2f x %.2f é %.2f", x, y, calc.multiplicar()), "Calculadora simples", JOptionPane.INFORMATION_MESSAGE); // INFORMATION é um icone no caso "i"
+        } 
+        if (cbxOperacao.getSelectedItem().equals("Divisão")) { 
+            JOptionPane.showMessageDialog(null, String.format("O resultado de %.2f x %.2f é %.2f", x, y, calc.dividir()), "Calculadora simples", JOptionPane.INFORMATION_MESSAGE); // INFORMATION é um icone no caso "i"
+        }
+
+        } catch (NumberFormatException ex){
+            JOptionPane.showMessageDialog(null, "Favor informar apenas números!", "Erro!!!",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
---
+++ trabalho02/Calculadora-Simples/grafica/FrmCalculadoraSimples.java
@@ -0,0 +1,40 @@
+package grafica;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Dimension;
+ 
+public class FrmCalculadoraSimples extends JFrame {
+    private JTextField txtx, txty;
+    private JComboBox<String> cbxOperacao;
+    private JButton btnCalcular;
+    
+    //construtora
+    public FrmCalculadoraSimples(){
+        setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10)); // Da esquerda para a direita 
+        setLocationRelativeTo(null); // Centro da tela 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quando clicar no x
+        setSize(new Dimension(320, 120));
+        
+        //componentes 
+        add(new JLabel("x:"));
+        add(txtx = new JTextField(10));
+         
+        add(new JLabel("y:"));
+        add(txty = new JTextField(10));
+
+        //vetor para carregar o combobox
+        String operacoes[] = {"Soma", "Subtração", "Multiplicação", "Divisão"};
+        add(new JLabel("Operações:"));
+        add(cbxOperacao = new JComboBox<String>(operacoes)); // Vai guardar itens do tipo string
+        add(btnCalcular = new JButton("Calcular"));
+
+        // Criar e vincular a action ao botão
+        ActionCalcular action = new ActionCalcular(txtx, txty, cbxOperacao);
+        btnCalcular.addActionListener(action);
+    }
+}
